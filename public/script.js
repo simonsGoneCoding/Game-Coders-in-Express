2691dcf823bf4bf375821dac33f5121e8d8a820b
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-02-16 18:03:00 +0000

    Updated files architecture

--- public/script.js
+++ public/script.js
@@ -1,15 +1,10 @@
 const question = document.querySelector("#question");
-const answer1 = document.querySelector("#answer1");
-const answer2 = document.querySelector("#answer2");
-const answer3 = document.querySelector("#answer3");
-const answer4 = document.querySelector("#answer4");
 
 function fillQuestionElement(data) {
   question.innerText = data.question;
-  answer1.innerText = data.answers[0];
-  answer2.innerText = data.answers[1];
-  answer3.innerText = data.answers[2];
-  answer4.innerText = data.answers[3];
+  data.answers.forEach((answer, index) => {
+    document.querySelector(`#answer${index + 1}`).innerText = answer;
+  });
 }
 
 function showNextQuestion() {
---
+++ routes/game.js
@@ -0,0 +1,41 @@
+function gameRoutes(app) {
+  let goodAnswers = 0;
+  let callAFriendUsed = false;
+  let askTheAudienceUsed = false;
+  let fiftyFiftyUsed = false;
+
+  const questions = [
+    {
+      question: "When is Earth closest to the Sun?",
+      answers: ["Autum", "Winter", "Summer", "Spring"],
+      correctAnswer: 1
+    },
+    {
+      question: "How was first ever discovered neutron star named?",
+      answers: ["Sirius", "MZ78", "Sagitarius A", "LGM (little green man)"],
+      correctAnswer: 3
+    },
+    {
+      question: "What is the smallest particle of an atom?",
+      answers: ["Electron", "Proton", "Quark", "Neutron"],
+      correctAnswer: 2
+    }
+  ];
+
+  app.get("/question", (req, res) => {
+    if (goodAnswers === questions.length) {
+      res.json({
+        winner: true
+      });
+    } else {
+      const nextQuestion = questions[goodAnswers];
+      const { question, answers } = nextQuestion;
+      res.json({
+        question,
+        answers
+      });
+    }
+  });
+}
+
+module.exports = gameRoutes;
